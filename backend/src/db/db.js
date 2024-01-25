eaf3aa3452b5cfd5c39f4b855ffe7ee6640ee789
shoeb <shoebtamboli98@gmail.com>
2024-01-25 13:05:02 +0530

    added mongoDB atlas

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -9,5 +9,5 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Could not connect to MongoDB", err));
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Could not connect to MongoDB Atlas", err));
